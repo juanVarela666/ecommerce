f1316dfbd3f56f2a81e9d84961a859b09baa4411
juanVarela666 <juanvc666@hotmail.com>
2024-11-04 21:33:09 -0600

    crear el repository y las clases de servicio y el metodo show de ProductoController

---
+++ src/main/java/com/ecommerce/juan/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.juan.repository;
+
+import com.ecommerce.juan.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+}
